95748d87ccd7a7e98cfeac2cd41cc8c65f43eb67
llucasll <llucasll.k@hotmail.com>
2017-12-05 17:59:27 -0200

    Update

--- src/3.partida.c
+++ src/3.partida.c
@@ -2,7 +2,7 @@
 
 #include "3.partida.h"
 
-void printBola(Bola *p);
+void printBola(byte cor, int x, int y);
 
 void partidaView(){
 	SDL_Rect srcRect, dstRect;
@@ -19,44 +19,31 @@ void partidaView(){
 							fundo.R, fundo.G, fundo.B ));
 */
 
-	srcRect.x = 0; srcRect.y = 0;
-	srcRect.w = tam.bola.x;
-	srcRect.h = tam.bola.y;
-	dstRect.x = tiro.x;
-	dstRect.y = tiro.y;
-
-
 	for(int i=0;i<linhas;i++)
 		for(int j=0;j<colunas;j++)
 			if(matriz[i][j].existe)
-				printBola(&matriz[i][j]);
-
-	if( SDL_BlitSurface( sprites[tiro.cor], &srcRect,
-								 screenSurface, &dstRect ) < 0 ) {
-		logger( "SDL could not blit! SDL Error: %s\n", SDL_GetError() );
-		quit = true;
-	}
-
+				printBola(matriz[i][j].cor, matriz[i][j].x, matriz[i][j].y);
+	
+	printBola(tiro.cor, tiro.x, tiro.y);
 
 	//Update the surface
 	SDL_UpdateWindowSurface( janela );
 }
 
-void printBola(Bola *p){
+void printBola(byte cor, int x, int y){
 	SDL_Rect srciRect, dstiRect;
 	Surface gprintSurface;
 
 	srciRect.x = 0; srciRect.y = 0;
 	srciRect.w = tam.bola.x;
 	srciRect.h = tam.bola.y;
-	dstiRect.x = p->x;
-
-	dstiRect.y = p->y;
-	gprintSurface=sprites[p->cor];
+	dstiRect.x = x;
+	dstiRect.y = y;
+	gprintSurface = sprites[cor];
 
 	if( SDL_BlitSurface( gprintSurface, &srciRect,
 						 screenSurface, &dstiRect ) < 0 ) {
-		printf( "SDL could not blit! SDL Error: %s\n", SDL_GetError() );
+		logger( "SDL could not blit! SDL Error: %s\n", SDL_GetError() );
 		quit = true;
 	}
 }
